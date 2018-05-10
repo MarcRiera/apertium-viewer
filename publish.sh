6e3382f32b63fbdc3a372335f7af8de5d225f5d5
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2018-05-10 15:16:57 +0200

    Fix for not having the compiled lttoolbox.jar in lib/

--- publish.sh
+++ publish.sh
@@ -2,9 +2,11 @@ ant clean
 
 ant jar
 
-cp apertium-viewer.jar ../../builds/apertium-viewer/
-scp apertium-viewer.jar j:javabog.dk/filer/apertium/
+#cp dist/apertium-viewer.jar ../../builds/apertium-viewer/
+scp dist/apertium-viewer.jar j:javabog.dk/filer/apertium/
 
-echo Commit and update apertium-viewer-newest-version.txt ?
-svn commit ../../builds/apertium-viewer/
+#echo Commit and update apertium-viewer-newest-version.txt ?
+#svn commit ../../builds/apertium-viewer/
+echo Update apertium-viewer-newest-version.txt ?
+read i
 scp apertium-viewer-newest-version.txt j:javabog.dk/filer/apertium/
